dd806bdc06f99a21e05795cdf0391fa102f69954
nguyenanhphat95 <nguyenanhphat95@gmail.com>
2022-03-07 11:25:24 +0700

    update lock user service

---
+++ interfaces/LockUser/IUpdateNumberLoginFail.ts
@@ -0,0 +1,4 @@
+export interface UpdateNumberLoginFailRequest {
+  username: string;
+  value: number;
+}
--- services/lockUserService.ts
+++ services/lockUserService.ts
@@ -1,9 +1,30 @@
 import axios, { AxiosResponse } from "axios";
 import { v4 as uuidv4 } from "uuid";
 
-export const getLoginFailApi = async () => {
+export const getNumberLoginFailApi = async () => {
   const resp: AxiosResponse<any> = await axios.post(
-    "/sso/api/LockUser/getLoginFail"
+    "/sso/api/LockUser/getNumberLoginFail"
+  );
+  return resp;
+};
+
+export const updateNumberLoginFailApi = async () => {
+  const resp: AxiosResponse<any> = await axios.post(
+    "/sso/api/LockUser/updateNumberLoginFail"
+  );
+  return resp;
+};
+
+export const getNumberOTPFailApi = async () => {
+  const resp: AxiosResponse<any> = await axios.post(
+    "/sso/api/LockUser/getNumberOTPFail"
+  );
+  return resp;
+};
+
+export const updateNumberOTPFailApi = async () => {
+  const resp: AxiosResponse<any> = await axios.post(
+    "/sso/api/LockUser/updateNumberOTPFail"
   );
   return resp;
 };
